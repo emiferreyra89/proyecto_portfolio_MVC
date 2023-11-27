ac0a692439d29b1c453f917a0383b079adf9d7c8
emiferreyra89 <ferreyra89@gmail.com>
2023-11-26 23:54:34 -0300

    agregue bloque de codigo en el entry point para el ruteo a las paginas home y about

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require ('express');
 const app = express();
+const path = require ('path');
+
+app.use(express.static(path.join(__dirname + '/public')))
+
+const homeController = require ('./controllers/homeController.js');
+const aboutController = require ('./controllers/aboutController.js');
+const homeRouter = require ('./routers/homeRouter.js');
+const aboutRouter = require ('./routers/homeRouter.js')
+
+app.use('/', homeController.home);
+app.use('/about', aboutController.about)
 
 app.listen (3000, () => {
     console.log('Servidor funcionando');
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,7 @@
 const path = require ('path');
 
 const homeController = {
-    home: (req,res) => {res.sendfile(path.join(__dirname + '../views/home.html'))}
+    home: (req,res) => {res.sendfile(path.join(__dirname , '../views/home.html'))}
 };
 
 module.exports = homeController;
